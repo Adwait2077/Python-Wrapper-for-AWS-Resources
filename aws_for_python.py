f485f9db700e168e980ce9afd55106b334a5fa4e
Adwait Sawant <93225829+Adwait2077@users.noreply.github.com>
2024-02-02 18:00:46 +0000

    Update aws_for_python.py

--- aws_for_python.py
+++ aws_for_python.py
@@ -3,16 +3,17 @@ import boto3
 region = 'your region'
 
 s3 = boto3.resource('s3')
-file_name='Your File Name'
-bucket_name='Your Bucket Name'
-key_name='The name with which it will be stored in S3 Bucket'
 
 def show_buckets(s3):
     for bucket in s3.buckets.all():
         print(bucket.name)
 show_buckets(s3)
 
-#For Uploading files from S3 Bucket
+#For Uploading files to the S3 Bucket
+file_name='Your File Name'       #The name with which the file is stored on your local machine
+bucket_name='Your Bucket Name'   ##Name of the Bucket to which the file will be uploaded
+key_name='StoredNameInS3Bucket'  #The name with which it will be stored in S3 Bucket
+
 def upload_to_bucket(s3, file_name, bucket_name, key_name):
     print("Uploading file to S3")
     data = open(file_name, 'rb')
@@ -21,10 +22,10 @@ def upload_to_bucket(s3, file_name, bucket_name, key_name):
 upload_to_bucket(s3, file_name, bucket_name, key_name)
 
 
-#For Downloading files from S3 Bucket
-file_name2='The name with which you want to store the file on your local machine'
-bucket_name2='Name of the Bucket from which the file will be downloaded'
-key_name2='Actual name of the file which needs to be downloaded'
+#For Downloading files from the S3 Bucket
+file_name2='LocalStoredFileName'  #The name with which you want to store the file on your local machine
+bucket_name2='Name of the Bucket' #Name of the Bucket from which the file will be downloaded
+key_name2='StoredNameInS3Bucket'  #Actual name of the file that needs to be downloaded
 
 def download_from_bucket(s3, file_name2, bucket_name2, key_name2):
     print("Downloading file from S3")
